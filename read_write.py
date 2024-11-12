c38db5e9192e9a8855034add44bbfd9e1eb7bab5
Pandananana <andreaswendelboe.dk@gmail.com>
2024-11-12 15:01:33 +0100

    Added motor movement function

--- read_write.py
+++ read_write.py
@@ -29,6 +29,8 @@
 #
 
 import os
+import numpy as np
+import time
 
 if os.name == 'nt':
     import msvcrt
@@ -57,16 +59,24 @@ ADDR_MX_PRESENT_POSITION   = 36
 PROTOCOL_VERSION            = 1.0               # See which protocol version is used in the Dynamixel
 
 # Default setting
-DXL_ID                      = 1                 # Dynamixel ID : 1
+DXL_ID                      = 3                 # Dynamixel ID : 1
 BAUDRATE                    = 1000000             # Dynamixel default baudrate : 57600
 DEVICENAME                  = '/dev/ttyACM0'    # Check which port is being used on your controller
                                                 # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
 
 TORQUE_ENABLE               = 1                 # Value for enabling the torque
 TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 10           # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 1000            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
-DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
+DXL_MINIMUM_POSITION_VALUE  = 0           # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 1023            # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 5                # Dynamixel moving status threshold
+
+# Motor range found from live test
+MOTOR_RANGE = {
+    1: [0, 1023],
+    2: [200, 900],
+    3: [50, 950],
+    4: [150, 850],
+}
 
 index = 0
 dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]         # Goal position
@@ -81,6 +91,15 @@ portHandler = PortHandler(DEVICENAME)
 # Set the protocol version
 # Get methods and members of Protocol1PacketHandler or Protocol2PacketHandler
 packetHandler = PacketHandler(PROTOCOL_VERSION)
+
+
+def set_motor_pos(motor_id, value):
+    # Write goal position
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, motor_id, ADDR_MX_GOAL_POSITION, value)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
 
 # Open port
 if portHandler.openPort():
@@ -109,37 +128,14 @@ elif dxl_error != 0:
     print("%s" % packetHandler.getRxPacketError(dxl_error))
 else:
     print("Dynamixel has been successfully connected")
-
-while 1:
-    print("Press any key to continue! (or press ESC to quit!)")
-    if getch() == chr(0x1b):
-        break
-
-    # Write goal position
-    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, dxl_goal_position[index])
-    if dxl_comm_result != COMM_SUCCESS:
-        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-    elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-    while 1:
-        # Read present position
-        dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
-        if dxl_comm_result != COMM_SUCCESS:
-            print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-        elif dxl_error != 0:
-            print("%s" % packetHandler.getRxPacketError(dxl_error))
-
-        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID, dxl_goal_position[index], dxl_present_position))
-
-        if not abs(dxl_goal_position[index] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
-            break
-
-    # Change goal position
-    if index == 0:
-        index = 1
-    else:
-        index = 0
+    
+set_motor_pos(1, int(MOTOR_RANGE[1][0] +  (MOTOR_RANGE[1][1] - MOTOR_RANGE[1][0]) / 2))
+time.sleep(0.5)
+set_motor_pos(2, int(MOTOR_RANGE[2][0] +  (MOTOR_RANGE[2][1] - MOTOR_RANGE[2][0]) / 2))
+time.sleep(0.5)
+set_motor_pos(3, int(MOTOR_RANGE[3][0] +  (MOTOR_RANGE[3][1] - MOTOR_RANGE[3][0]) / 2))
+time.sleep(0.5)
+set_motor_pos(4, int(MOTOR_RANGE[4][0] +  (MOTOR_RANGE[4][1] - MOTOR_RANGE[4][0]) / 2))
 
 
 # Disable Dynamixel Torque
