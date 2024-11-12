8c972c56ca2a4dc0bd19d9d7883b33653aa03dd7
Pandananana <andreaswendelboe.dk@gmail.com>
2024-11-12 15:25:28 +0100

    Degrees to motor position done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import dynamixel_sdk as dxl
+import time
+import numpy as np
+from dynamixel_sdk import *
+
 ADDR_MX_TORQUE_ENABLE         = 24
 ADDR_MX_CW_COMPLIANCE_MARGIN  = 26
 ADDR_MX_CCW_COMPLIANCE_MARGIN = 27
@@ -14,14 +17,53 @@ DEVICENAME                    = '/dev/ttyACM0'
 BAUDRATE                      = 1000000
 TORQUE_ENABLE                 = 1
 TORQUE_DISABLE                = 0
-portHandler = dxl.PortHandler(DEVICENAME)
-packetHandler = dxl.PacketHandler(PROTOCOL_VERSION)
+portHandler = PortHandler(DEVICENAME)
+packetHandler = PacketHandler(PROTOCOL_VERSION)
 portHandler.openPort()
 portHandler.setBaudRate(BAUDRATE)
 for DXL_ID in DXL_IDS:
     packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
-    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_MARGIN, 0)
-    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_MARGIN, 0)
-    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_SLOPE, 32)
-    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_SLOPE, 32)
-    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 10)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_MARGIN, 5)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_MARGIN, 5)
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_SLOPE, 128)
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_SLOPE, 128)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 1023)
+
+# Motor range found from live test
+MOTOR_RANGE = {
+    1: [0, 1023],
+    2: [200, 900],
+    3: [50, 950],
+    4: [150, 850],
+}
+
+MOTOR_VALUE_DEGREES = 300.0 / 1023.0
+
+def set_motor_pos(motor_id, degrees):
+    center = 1023 / 2
+    value = int(center + degrees / MOTOR_VALUE_DEGREES)
+    # Write goal position
+    dxl_comm_result, dxl_error = packetHandler.write2ByteTxRx(portHandler, motor_id, ADDR_MX_GOAL_POSITION, value)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+
+set_motor_pos(1, 45)
+set_motor_pos(2, 45)
+set_motor_pos(3, 45)
+set_motor_pos(4, 45)
+
+
+
+time.sleep(1)
+# Disable Dynamixel Torque
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+# Close port
+portHandler.closePort()
