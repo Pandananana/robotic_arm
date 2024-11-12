469840c460322b9377189c6261074c0a3315ba63
mikestrate <mikestrate12@gmail.com>
2024-11-12 16:59:25 +0100

    Q10%Q9

--- matlab/Config4DOF.m
+++ matlab/Config4DOF.m
@@ -11,7 +11,7 @@ function [q1,q2,q3,q4] = Config4DOF(o4,x4_z,d1,a2,a3,a4)
     r=sqrt(oc(1)^2+oc(2)^2);
     c3 = (r^2 + s^2 - a2^2-a3^2)/(2*a2*a3);
 
-    q3 = atan2(sqrt(1-c3^2), c3);
+    q3 = atan2(-sqrt(1-c3^2), c3);
     q2 = atan2(s,r) - atan2(a3*sin(q3),a2+a3*c3);
     q4 = asin(x4_z) - (q2 + q3);
 end
